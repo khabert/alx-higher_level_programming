6385e8d0ccf769a9d9d8e7a4b466e7ead21c99bd
khabert <khabertzion11@gmail.com>
2023-05-24 00:49:59 -0700

    new move to

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,19 @@
 -- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Use the database
+USE hbtn_0c_0;
 
--- Convert the field to UTF8
-ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Check if the table exists
+SELECT COUNT(*)
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Convert the "name" field to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
