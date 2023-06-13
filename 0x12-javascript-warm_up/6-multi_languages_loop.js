e45038ffeac1b588c22b5713cc80c8fe5a6cf4c9
khabert <khabertzion11@gmail.com>
2023-06-12 23:59:16 -0700

    multi_languages_loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let s;
-for (s in strings) {
-  console.log(strings[s]);
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const language of languages) {
+  console.log(language);
 }
