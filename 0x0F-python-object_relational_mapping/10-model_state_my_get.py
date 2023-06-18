4432b31b7e344701f636ceb06dcf559f37859a8f
khabert <khabertzion11@gmail.com>
2023-06-18 14:06:48 -0700

    correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-Usage: ./11-model_state_insert.py <mysql username> /
+"""Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <mysql username> /
                                   <mysql password> /
                                   <database name>
+                                  <state name searched>
 """
 
 import sys
@@ -17,7 +19,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
-    session.commit()
-    print(louisiana.id)
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Changes the name of a State object from the database hbtn_0e_6_usa
+"""Changes the name of the State object with id = 2 to
+New Mexico in the database hbtn_0e_6_usa.
+Usage: ./12-model_state_update_id_2.py <mysql username> /
+                                       <mysql password> /
+                                       <database name>
 """
+
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create engine and bind session
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Get the State object with id = 2
-    state = session.query(State).get(2)
-
-    if state:
-        # Change the name of the State
-        state.name = "New Mexico"
-        session.commit()
-
-    session.close()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
